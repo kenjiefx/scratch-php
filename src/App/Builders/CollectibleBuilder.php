d4956697ecded54f30f65bb48d630eb476f47893
Kenjie Terrado <terradoflex@gmail.com>
2025-06-27 08:21:52 +0800

    fix: should collect assets only once per page

--- src/App/Builders/CollectibleBuilder.php
+++ src/App/Builders/CollectibleBuilder.php
@@ -13,6 +13,10 @@ use Kenjiefx\ScratchPHP\App\Components\ComponentCollector;
 use Kenjiefx\ScratchPHP\App\Components\ComponentRegistry;
 use Kenjiefx\ScratchPHP\App\Themes\ThemeModel;
 
+/**
+ * CollectibleBuilder is responsible for collecting component and block 
+ * assets (CSS and JS) for a page in a theme.
+ */
 class CollectibleBuilder {
 
     public function __construct(
@@ -30,6 +34,11 @@ class CollectibleBuilder {
         string $event
     ): string {
         $content = $this->assetCollector->collectAll($assetEnum, $themeModel);
+
+        // Determines whether the content has already been collected for each file 
+        // This is done to avoid duplicate processing of the same CSS or JS file
+        // This is particularly useful when multiple components share the same file.
+        $hasCollectedColletible = [];
     
         foreach ($this->componentRegistry->get() as $componentModel) {
             [$createCollectibleMethod, $eventClass] = match ($event) {
@@ -44,12 +53,29 @@ class CollectibleBuilder {
             );
     
             $collectible = $this->componentCollector->{$createCollectibleMethod}($componentModel, $themeModel);
-            $originalContent = $this->fileService->readFile($collectible->file);
-    
-            $eventObject = new $eventClass($componentModel, $componentDir, $originalContent);
-            $this->eventDispatcher->dispatchEvent($eventObject);
-    
-            $content .= $eventObject->getContent();
+
+            // Check if the file has already been collected
+            $filePath = $collectible->file->path;
+            if (!isset($hasCollectedColletible[$filePath])) {
+
+                $originalContent = "";
+
+                // Throw no error if the file does not exist, just use an empty string
+                if ($this->fileService->fileExists($collectible->file)) {
+                    $originalContent = $this->fileService->readFile($collectible->file);
+                }
+
+                // Dispatch the event and let the event listener modify the content if needed
+                $eventObject = new $eventClass($componentModel, $componentDir, $originalContent);
+                $this->eventDispatcher->dispatchEvent($eventObject);
+
+                // Append the content to the final output
+                $content .= $eventObject->getContent();
+
+                // Mark this file as collected to avoid processing it again
+                $hasCollectedColletible[$filePath] = true;
+            }
+        
         }
     
         return $content;
