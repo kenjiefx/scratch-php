78e66eaef5fb7546ca35614f94132f5a04b737c8
Kenjie Terrado <terradoflex@gmail.com>
2025-07-20 21:32:00 +0800

    dist exporter service

---
+++ src/App/Implementations/DistExporter/DistExportService.php
@@ -0,0 +1,30 @@
+<?php 
+
+namespace Kenjiefx\ScratchPHP\App\Implementations\DistExporter;
+
+use Kenjiefx\ScratchPHP\App\Interfaces\ExportServiceInterface;
+use Kenjiefx\ScratchPHP\App\Pages\PageContent;
+use Kenjiefx\ScratchPHP\App\Pages\PageModel;
+
+class DistExportService implements ExportServiceInterface {
+
+    public function __construct(
+
+    ) {}
+
+    public function exportPageContents(PageContent $pageContent, PageModel $pageModel): void {
+
+    }
+
+    public function createPageAssetPath(PageModel $pageModel, string $assetType): string {
+        $pageUrl = $pageModel->urlPath;
+        // Remove .html from page path
+        $pageUrl = preg_replace('/\.html$/i', '', $pageUrl);
+        // Append asset type
+        $assetPath = '/assets/' . $pageUrl . '.' . $assetType;
+        // Remove double slashes if they exist
+        $assetPath = str_replace('//', '/', $assetPath);
+        return $assetPath;
+    }
+
+}
