396236a771ff1e8dc6ca3316a501e37b2b53991c
Kenjie Terrado <terradoflex@gmail.com>
2025-07-22 15:44:39 +0800

    refactor: template model

--- src/App/Templates/TemplateModel.php
+++ src/App/Templates/TemplateModel.php
@@ -1,14 +1,18 @@
-<?php
+<?php 
 
 namespace Kenjiefx\ScratchPHP\App\Templates;
 
-class TemplateModel
-{
+/**
+ * TemplateModel class represents a template with a name.
+ */
+class TemplateModel {
 
     public function __construct(
+        /**
+         * The name of the template.
+         * @var string
+         */
         public readonly string $name
-    ){
-        
-    }
+    ) {}
 
 }
