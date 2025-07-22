2c72f423f4b038159a3e3cb896760c6e5422a91f
Kenjie Terrado <terradoflex@gmail.com>
2025-07-22 15:45:03 +0800

    refactor: theme model

--- src/App/Themes/ThemeModel.php
+++ src/App/Themes/ThemeModel.php
@@ -2,10 +2,19 @@
 
 namespace Kenjiefx\ScratchPHP\App\Themes;
 
+/**
+ * ThemeModel represents a theme in the application.
+ */
 class ThemeModel {
-    
+
     public function __construct(
+
+        /**
+         * The name of the theme.
+         * @var string
+         */
         public readonly string $name
+        
     ) {}
 
 }
