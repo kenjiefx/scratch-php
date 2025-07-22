ae604552634bc59e34ebf93d25be1ba30ae083e1
Kenjie Terrado <terradoflex@gmail.com>
2025-07-22 15:30:39 +0800

    refactor: block domain objects

--- src/App/Blocks/BlockFactory.php
+++ src/App/Blocks/BlockFactory.php
@@ -2,21 +2,32 @@
 
 namespace Kenjiefx\ScratchPHP\App\Blocks;
 
+use Kenjiefx\ScratchPHP\App\Utils\UniqueIdGenerator;
+
 class BlockFactory {
 
-    private static int $idIncrementor = 1;
+    public function __construct(
+        private UniqueIdGenerator $uniqueIdGenerator
+    ) {}
 
     /**
      * Creates a new BlockModel instance.
      *
      * @param string $name The name of the block.
+     * @param array $data The data associated with the block.
      * @return BlockModel The created BlockModel instance.
      */
-    public static function create(string $name): BlockModel {
-        $componentId = strval(self::$idIncrementor++).uniqid();
+    public function create(string $name, array $data): BlockModel {
+        $uniqueId = $this->uniqueIdGenerator->generate();
+        $blockData = new BlockData();
+        foreach ($data as $key => $value) {
+            $blockData[$key] = $value;
+        }
         return new BlockModel(
-            $name,
-            $componentId
+            id: $uniqueId,
+            name: $name,
+            data: $blockData
         );
     }
+
 }
